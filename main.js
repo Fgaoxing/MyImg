6da3bb21f3b10cb6cf9ef75ffe2302cbd210cdfb
crsn <92655031+Fgaoxing@users.noreply.github.com>
2023-02-19 02:14:10 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+import fetch from 'node-fetch';
+import fs from 'fs';
+
+fetch('https://registry.npmmirror.com/ytblogimg/').then(function (res) {
+    res.json().then(function (json) {
+        if ('dist-tags' in json){
+            console.log(json['dist-tags']['latest']);
+            if (json['dist-tags']['latest'].indexOf('-')===-1){
+                let v = parseInt(json['dist-tags']['latest'].split('.').join(''))
+                v++;
+                let file = JSON.parse(fs.readFileSync('./package.json'))
+                file['version'] = v.toString().split('').join('.');
+                fs.writeFileSync('./package.json', JSON.stringify(file, '', '\t'))
+            }
+        }
+    }).catch(function (err) {
+        console.log(err);
+    })
+}).catch(function (err) {
+    console.log(err);
+})
