04fb347e096076fba9a95abc8e56f03c7a8ac352
Fgaoxing <92655031+Fgaoxing@users.noreply.github.com>
2024-02-29 18:20:43 +0800

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ ss.http({
     ":(.*)": function (req, res) {
         fetch('https://registry.npmmirror.com/ytblogimg/').then(data => data.json()).then(function (json) {
             if ('dist-tags' in json) {
-                console.log(json['dist-tags']['latest']);
+                console.log(json['dist-tags']['latest'], `..${decodeURIComponent(req.url.pathname)}`);
                 if (!fs.existsSync(`..${decodeURIComponent(req.url.pathname)}`)) {
                     res.writeHead(404, { "Content-Type": "text/html; charset=utf-8" });
                     res.end(`<html lang="zh-cn"><head><meta http-equiv="content-type" content="text/html; charset=utf-8"><title>404 图片不存在 - 图床</title><link rel="shortcut icon" href="/webp/logo.webp"><link rel="stylesheet" href="https://jsd.onmicrosoft.cn/gh/Hexo-theme-Jian/static/css/grid.css"></head><body><div class="card center" style="width: calc(100% - 32px);display: flex;justify-content: center;max-width: 300px;flex-direction: column;align-items: center;"><h1 style="font-size: 50px;margin: 15px;">404</h1><span style="margin: 25px;">图片不存在，请检查URL</span></div></body></html>`);
